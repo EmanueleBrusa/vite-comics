90d2aa9f8ed6a5460bedd99218b689a7e4a20a5f
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-06-22 11:25:21 +0200

    push pre-ticket

---
+++ src/components/AppCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props:{
+        mybooks: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="mybooks.thumb">
+        <h4>{{ mybooks.series }}</h4>
+        <p><strong>Type: {{ mybooks.type }}</strong></p>
+    </div>
+</template>
+
+<style lang="sccss" scoped>
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,100 @@
 <script>
+import AppCard from './AppCard.vue';
 
+export default {
+    components: { 
+        AppCard,
+    },
+    data() {
+        return {
+            books: [
+            {
+                thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                price: "$19.99",
+                series: "Action Comics",
+                type: "comic book"
+            },
+            {
+                thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                price: "$3.99",
+                series: "American Vampire 1976",
+                type: "comic book"
+            },
+            {
+                thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                price: "$16.99",
+                series: "Aquaman",
+                type: "graphic novel"
+            },
+            {
+                thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                price: "$2.99",
+                series: "Batgirl",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                price: "$3.99",
+                series: "Batman",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                price: "$2.99",
+                series: "Batman Beyond",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                price: "$3.99",
+                series: "Batman/Superman",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                price: "$4.99",
+                series: "Batman/Superman Annual",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                price: "$5.99",
+                series: "Batman: The Joker War Zone",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                price: "$6.99",
+                series: "Batman: Three Jokers",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                price: "$4.99",
+                series: "Batman: White Knight Presents: Harley Quinn",
+                type: "comic book"
+            },
+            {
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                price: "$16.99",
+                series: "Catwoman",
+                type: "graphic novel"
+            }
+        ]
+    }
+    }
+}
 </script>
 
-<template lang="">
+<template>
 <div id="jumbotron">
 
 </div>
 <div class="container">
     <div class="row">
-        <h1>METTICI QUI LA ROBA PER DOMANI</h1>
+        <AppCard v-for="(comic, index) in books" :key="index" :mybooks="comic"/>
+        <button id="load">LOAD MORE</button>
+        <button id="current">CURRENT SERIES</button>           
     </div>
 </div>
 </template>
@@ -20,10 +106,40 @@ h1{
 }
 .container{
     background-color: black;
-    display: flex;
-    justify-content: center;
-    padding: 50px;
-    width: 100vw;
+    padding: 20px 0;
+    .row{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        position: relative;
+        padding-top: 20px;
+        h2{
+            color: #fff;
+        }
+        .card{
+            width: calc(100% / 6);
+        }
+        button#current{
+            color: #fff;
+            background-color: #0074e8;
+            position: absolute;
+            top: -50px;
+            left: 0;
+            padding: 12px 50px;
+            font-size: 20px;
+            font-weight: 700;
+            border: #0074e8;
+        }
+        button#load{
+            color: #fff;
+            background-color: #0074e8;
+            padding: 10px 45px;
+            font-size: 12px;
+            font-weight: 700;
+            border: #0074e8;
+            margin-top: 40px;
+        }
+    }
 }
 #jumbotron{
     background-image: url(../../public/jumbotron.jpg);
